a4e6b12546686f05aff5df207408f8434c21517b
Reid Paape <wrpaape@gmail.com>
2017-09-21 23:21:48 -0500

    clearing out ring buffer code from header

--- include/lock_free_queue/lock_free_queue.hpp
+++ include/lock_free_queue/lock_free_queue.hpp
@@ -43,39 +43,6 @@ public:
                                       >::value
                                   )
     {
-        // advance head
-        T *next_head;
-        T *next_tail;
-
-        T *const current_head = head.load(std::memory_order_relaxed);
-
-        if (current_head == last) {
-            next_head = first;
-            next_tail = next_head + 1;
-
-        } else {
-            next_head = current_head + 1;
-            next_tail = (next_head == last) ? first : (next_head + 1);
-        }
-
-        // copy next_head into temp variable in case CAS fails and resets
-        T *overlap = next_head;
-
-        // advance tail if next_head would clobber
-        if (tail.compare_exchange_strong(overlap,
-                                         next_tail,
-                                         std::memory_order_relaxed,
-                                         std::memory_order_relaxed))
-            overlap->~T(); // make room for next head
-
-        (void) new(current_head) T(std::forward<Args>(args)...);
-
-        // ensure construction writes committed before advancing head
-        std::atomic_thread_fence(std::memory_order_release);
-
-        // advance head
-        head.store(next_head,
-                   std::memory_order_relaxed);
     }
 
 
@@ -112,167 +79,25 @@ private:
     inline enable_if_t<IsNothrowMoveConstructibleAndDestructible, bool>
     try_pop_back(T &value) noexcept
     {
-        T *current_tail;
-
-        // acquire tail
-        do {
-            current_tail = tail.exchange(nullptr,
-                                         std::memory_order_relaxed);
-        } while (current_tail == nullptr);
-
-        const bool not_empty = (   current_tail
-                                != head.load(std::memory_order_relaxed));
-
-        // release tail ASAP
-        if (not_empty) {
-            // advance tail
-            tail.store((current_tail == last) ? first : (current_tail + 1),
-                       std::memory_order_relaxed);
-
-            // ensure tail is not accessed mid-construction (from an insertion)
-            std::atomic_thread_fence(std::memory_order_acquire);
-
-            value = std::move(*current_tail);
-
-            current_tail->~T();
-
-        } else {
-            // replace
-            tail.store(current_tail,
-                       std::memory_order_relaxed);
-        }
-
-        return not_empty;
     }
 
     template<bool IsNothrowMoveConstructibleAndDestructible>
     inline enable_if_t<!IsNothrowMoveConstructibleAndDestructible, bool>
     try_pop_back(T &value)
     {
-        T *current_tail;
-
-        // acquire tail
-        do {
-            current_tail = tail.exchange(nullptr,
-                                         std::memory_order_relaxed);
-        } while (current_tail == nullptr);
-
-        T *next_tail = current_tail;
-
-        const bool not_empty = (   current_tail
-                                != head.load(std::memory_order_relaxed));
-
-        if (not_empty) {
-            // ensure tail is not accessed mid-construction (from an insertion)
-            std::atomic_thread_fence(std::memory_order_acquire);
-
-            try {
-                value = std::move(*current_tail);
-
-                // advance tail
-                next_tail = (current_tail == last) ? first : (current_tail + 1);
-
-                current_tail->~T();
-
-            } catch (...) {
-                // advance tail if move succeeded
-                tail.store(next_tail,
-                           std::memory_order_relaxed);
-
-                throw; // reraise
-            }
-        }
-
-        // replace tail
-        tail.store(next_tail,
-                   std::memory_order_relaxed);
-
-        return not_empty;
-    }
-
-    inline static T *
-    allocate(const std::size_t capacity)
-    {
-        if (capacity == 0)
-            throw std::invalid_argument(
-                "LockFreeQueue::allocate() -- capacity must be nonzero"
-            );
-
-        T *const buffer = static_cast<T *>(
-            std::malloc(sizeof(T) * (capacity + 1))
-        );
-
-        if (!buffer)
-            throw std::bad_alloc();
-
-        return buffer;
     }
 
     template<bool IsNothrowDestructible>
     inline enable_if_t<IsNothrowDestructible>
     destroy() noexcept
     {
-        T *ptr;
-
-        T *const final_tail = tail.load(std::memory_order_relaxed);
-        T *const final_head = head.load(std::memory_order_relaxed);
-
-        if (final_tail <= final_head) {
-            for (ptr = final_tail; ptr < final_head; ++ptr)
-                ptr->~T();
-
-        } else {
-            for (ptr = first; ptr < final_head; ++ptr)
-                ptr->~T();
-
-            ptr = final_tail;
-            do {
-                ptr->~T();
-            } while (++ptr <= last);
-        }
-
-        std::free(static_cast<void *>(first));
     }
 
     template<bool IsNothrowDestructible>
     inline enable_if_t<!IsNothrowDestructible>
     destroy()
     {
-        try {
-            T *ptr;
-
-            T *const final_tail = final_tail.load(std::memory_order_relaxed);
-            T *const final_head = head.load(std::memory_order_relaxed);
-
-            if (final_tail <= final_head) {
-                for (ptr = final_tail; ptr < final_head; ++ptr)
-                    ptr->~T();
-
-
-            } else {
-                for (ptr = first; ptr < final_head; ++ptr)
-                    ptr->~T();
-
-                ptr = final_tail;
-                do {
-                    ptr->~T();
-                } while (++ptr <= last);
-            }
-
-        } catch (...) {
-            // ensure buffer is freed
-            std::free(static_cast<void *>(first));
-            throw; // reraise
-        }
-
-        std::free(static_cast<void *>(first));
     }
-
-
-    T         *const first;
-    T         *const last;
-    std::atomic<T *> head;
-    std::atomic<T *> tail;
 }; // class LockFreeQueue
 
 #endif // ifndef LOCK_FREE_QUEUE_LOCK_FREE_QUEUE_HPP
