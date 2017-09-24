0b26d9e5d5cb208d146d960df3ad92bbe6ca2a8c
Reid Paape <wrpaape@gmail.com>
2017-09-23 22:34:34 -0500

    node, allocator, head/tail

--- include/lock_free_queue/lock_free_queue.hpp
+++ include/lock_free_queue/lock_free_queue.hpp
@@ -12,92 +12,47 @@
 
 
 
-template<typename T>
+template<typename T, std::size_t Capacity>
 class LockFreeQueue
 {
+private:
+    struct Node
+    {
+        T                   value;
+        std::atomic<Node *> next;
+    }; // struct Node
+
+    class Allocator
+    {
+    public:
+        Node *
+        allocate()
+        {
+            return nullptr;
+        }
+
+        void
+        destroy(Node *const node)
+        {
+
+        }
+
+    private:
+        std::atomic<Node *> free;
+        char buffer[sizeof(Node) * capacity];
+    }; // class Allocator
+
 public:
-    LockFreeQueue(const std::size_t capacity)
-        : first(allocate(capacity)), // pointer to first element in buffer
-          last(first + capacity),    // pointer to last element in buffer
-          head(first),
-          tail(first)
+    LockFreeQueue()
     {}
 
-    ~LockFreeQueue() noexcept(std::is_nothrow_destructible<T>::value)
-    {
-        // ensure latest modifications have been loaded
-        std::atomic_thread_fence(std::memory_order_acquire);
-
-        destroy<
-            std::is_nothrow_destructible<T>::value
-        >();
-    }
-
-    template<typename ...Args>
-    void
-    emplace_front(Args &&...args) noexcept(
-                                      std::is_nothrow_destructible<T>::value
-                                   && std::is_nothrow_constructible<
-                                          T,
-                                          Args &&...
-                                      >::value
-                                  )
-    {
-    }
-
-
-    void
-    push_front(const T &value) noexcept(noexcept(emplace_front(value)))
-    {
-        emplace_front(value);
-    }
-
-    void
-    push_front(T &&value) noexcept(noexcept(emplace_front(std::move(value))))
-    {
-        emplace_front(std::move(value));
-    }
-
-    bool
-    try_pop_back(T &value) noexcept(
-                               std::is_nothrow_move_constructible<T>::value
-                            && std::is_nothrow_destructible<T>::value
-                           )
-    {
-        return try_pop_back<
-            std::is_nothrow_move_constructible<T>::value
-         && std::is_nothrow_destructible<T>::value
-        >(value);
-    }
-
+    ~LockFreeQueue()
+    {}
 
 private:
-    template<bool E, typename R = void>
-    using enable_if_t = typename std::enable_if<E, R>::type;
-
-    template<bool IsNothrowMoveConstructibleAndDestructible>
-    inline enable_if_t<IsNothrowMoveConstructibleAndDestructible, bool>
-    try_pop_back(T &value) noexcept
-    {
-    }
-
-    template<bool IsNothrowMoveConstructibleAndDestructible>
-    inline enable_if_t<!IsNothrowMoveConstructibleAndDestructible, bool>
-    try_pop_back(T &value)
-    {
-    }
-
-    template<bool IsNothrowDestructible>
-    inline enable_if_t<IsNothrowDestructible>
-    destroy() noexcept
-    {
-    }
-
-    template<bool IsNothrowDestructible>
-    inline enable_if_t<!IsNothrowDestructible>
-    destroy()
-    {
-    }
+    std::atomic<Node *> head;
+    std::atomic<Node *> tail;
+    Allocator           allocator;
 }; // class LockFreeQueue
 
 #endif // ifndef LOCK_FREE_QUEUE_LOCK_FREE_QUEUE_HPP
